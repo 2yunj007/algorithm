12a39acc8cf1feeb08958f14c0dfa6f8250b7766
yunzo <139421060+2yunj007@users.noreply.github.com>
2023-08-08 16:18:14 +0900

    [Silver V] Title: 최소공배수, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,19 @@
+A, B = map(int, input().split())
+
+num = max(A, B)
+f = min(A, B)
+max_f = 0   # 최대공약수
+
+# 유클리드 호제법
+while True:
+    # 나누어 떨어지게 하는 수가 최대공약수
+    if num % f == 0:    
+        max_f = f
+        break
+    # 나누어 떨어지지 않으면, 그 나머지로 현재 나누어지는 수(f)를 나눠야 함
+    else:
+        num, f = f, num % f
+    
+a = A // max_f
+b = B // max_f
+print(a * b * max_f)
