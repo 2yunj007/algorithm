cc003bbbc5003d529d617afb1cf2b84f8b992d27
yunzo <139421060+2yunj007@users.noreply.github.com>
2023-08-12 21:48:31 +0900

    [Silver IV] Title: 큐 2, Time: 1648 ms, Memory: 173932 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,39 @@
+import sys
+N = int(input())
+input = sys.stdin.readline
+queue = []
+bot = 0     # 큐의 시작 인덱스
+
+for i in range(N):
+    comd = input().split()
+    # case 1
+    if comd[0] == 'push':
+        queue.append(comd[1])
+    # case 2
+    elif comd[0] == 'pop':
+        if len(queue) > bot:    # 큐의 길이 > bot이면 원소가 남아 있다는 것
+            print(queue[bot])
+            bot += 1
+        else:
+            print(-1)
+    # case 3
+    elif comd[0] == 'size':
+        print(len(queue)-bot)
+    # case 4
+    elif comd[0] == 'empty':
+        if len(queue) > bot:
+            print(0)
+        else:
+            print(1)
+    # case 5
+    elif comd[0] == 'front':
+        if len(queue) > bot:
+            print(queue[bot])
+        else:
+            print(-1)
+    # case 6
+    elif comd[0] == 'back':
+        if len(queue) > bot:
+            print(queue[-1])
+        else:
+            print(-1)
