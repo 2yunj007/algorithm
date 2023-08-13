34cbcaefeb9860550dfed7e8dc093312a7030ca3
yunzo <139421060+2yunj007@users.noreply.github.com>
2023-08-13 12:36:46 +0900

    [Silver IV] Title: 덱 2, Time: 1068 ms, Memory: 102420 KB -BaekjoonHub

---
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -0,0 +1,49 @@
+from collections import deque
+import sys
+
+N = int(sys.stdin.readline())
+rear = 0
+front = 0
+dq = deque()
+
+for i in range(N):
+    cmd = sys.stdin.readline().split()
+    # case 1
+    if cmd[0] == '1':
+        dq.appendleft(cmd[1])
+    # case 2
+    elif cmd[0] == '2':
+        dq.append(cmd[1])
+    # case 3
+    elif cmd[0] == '3':
+        if len(dq) != 0:
+            print(dq.popleft())
+        else:
+            print(-1)
+    # case 4
+    elif cmd[0] == '4':
+        if len(dq) != 0:
+            print(dq.pop())
+        else:
+            print(-1)
+    # case 5
+    elif cmd[0] == '5':
+        print(len(dq))
+    # case 6
+    elif cmd[0] == '6':
+        if len(dq) != 0:
+            print(0)
+        else:
+            print(1)
+    # case 7
+    elif cmd[0] == '7':
+        if len(dq) != 0:
+            print(dq[0])
+        else:
+            print(-1)
+    # case 8
+    elif cmd[0] == '8':
+        if len(dq) != 0:
+            print(dq[-1])
+        else:
+            print(-1)
