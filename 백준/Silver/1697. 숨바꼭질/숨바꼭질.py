59958e4822e6bc321dc4cae5feddc8d72e2e41b2
yunzo <139421060+2yunj007@users.noreply.github.com>
2023-08-19 22:49:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 624 ms, Memory: 137788 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,6 +1,6 @@
 def bfs(N, K):
-    visited = {}
-    q = deque()
+    visited = {}    # 방문 여부를 확인할 때 시간 복잡도를 줄이기 위함
+    q = deque()     # pop(0) -> 시간 초과
     q.append(N)     # 시작 정점 인큐
     visited[N] = 0  # 시작 정점 방문 표시
 
@@ -8,6 +8,7 @@ def bfs(N, K):
         X = q.popleft()
         for w in [X-1, X+1, X*2]:
             # 아직 방문하지 않았다면 인큐 및 방문 표시
+            # w 범위에 대한 조건이 없으면 메모리 초과
             if 0 <= w < 10**6 and w not in visited:
                 q.append(w)
                 visited[w] = visited[X] + 1
