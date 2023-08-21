711eadd661d52a44f9681f286a02f3086ff5a806
yunzo <139421060+2yunj007@users.noreply.github.com>
2023-08-21 14:06:21 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 436 ms, Memory: 64,352 KB -BaekjoonHub

---
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
@@ -0,0 +1,10 @@
+T = int(input())
+for tc in range(1, T+1):
+    N, K = map(int,input().split())
+    students = list(map(int, input().split()))
+
+    print(f'#{tc}', end=' ')
+    for i in range(1, N+1):
+        if i not in students:
+            print(i, end=' ')
+    print()
