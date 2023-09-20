b00a6cd22cebd345811b11548d8aaf0a28dfcee0
yunzo <139421060+2yunj007@users.noreply.github.com>
2023-09-20 23:41:55 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+def f(i, N, M):
+    if i == M:
+        print(*path)
+        return
+
+    for j in range(1, N+1):
+        if j in path:
+            continue
+        if i > 0 and j < path[i-1]:
+            continue
+        path[i] = j
+        f(i + 1, N, M)
+        path[i] = 0
+
+
+N, M = map(int, input().split())
+path = [0] * M
+f(0, N, M)
