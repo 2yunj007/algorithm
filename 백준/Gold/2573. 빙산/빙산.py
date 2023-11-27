d6e1db0336f23efb9a3c47cadb58e14d6eb5242a
yunzo <139421060+2yunj007@users.noreply.github.com>
2023-11-28 02:01:04 +0900

    [Gold IV] Title: 빙산, Time: 2056 ms, Memory: 34916 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,60 +1,70 @@
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
 
 
-def bfs(x, y):
-    q = deque([(x, y)])
-    visited[x][y] = 1
-    seaList = []
+def bfs(si, sj):
+    q = deque()
+    visit[si][sj] = 1
+    q.append((si, sj))
+    cnt_lst = []
 
     while q:
-        x, y = q.popleft()
-        sea = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if not graph[nx][ny]:
-                    sea += 1
-                elif graph[nx][ny] and not visited[nx][ny]:
-                    q.append((nx, ny))
-                    visited[nx][ny] = 1
-        if sea > 0:
-            seaList.append((x, y, sea))
-    for x, y, sea in seaList:
-        graph[x][y] = max(0, graph[x][y] - sea)
+        i, j = q.popleft()
+        cnt = 0
+        for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            ni, nj = i + d[0], j + d[1]
+
+            if not(0 <= ni < N and 0 <= nj < M):
+                continue
+            if visit[ni][nj]:
+                continue
+            # 바다 개수 카운트
+            if not arr[ni][nj]:
+                cnt += 1
+                continue
+
+            q.append((ni, nj))
+            visit[ni][nj] = 1
+
+        # 녹일 빙산 리스트에 저장
+        if cnt:
+            cnt_lst.append((i, j, cnt))
+
+    # 빙산 녹이기, 0보다 작으면 0으로
+    for i, j, cnt in cnt_lst:
+        arr[i][j] = max(0, arr[i][j] - cnt)
 
     return 1
 
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+N, M = map(int, input().split())    # r c
+arr = [list(map(int, input().split())) for _ in range(N)]
 
+# 빙산 위치 저장
 ice = []
-for i in range(n):
-    for j in range(m):
-        if graph[i][j]:
+for i in range(N):
+    for j in range(M):
+        if arr[i][j]:
             ice.append((i, j))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 year = 0
-
 while ice:
-    visited = [[0] * m for _ in range(n)]
-    delList = []
-    group = 0
+    visit = [[0] * M for _ in range(N)]
+    melt_lst = []
+    n = 0
     for i, j in ice:
-        if graph[i][j] and not visited[i][j]:
-            group += bfs(i, j)
-        if graph[i][j] == 0:
-            delList.append((i, j))
-    if group > 1:
+        if arr[i][j] and not visit[i][j]:
+            n += bfs(i, j)
+        # 다 녹았으면 리스트에 저장
+        if not arr[i][j]:
+            melt_lst.append((i, j))
+    # 빙산 그룹이 2개 이상일 경우
+    if n > 1:
         print(year)
-        break
-    ice = sorted(list(set(ice) - set(delList)))
+        exit()
+    # 다 녹은 빙산 삭제 후 반복
+    ice = sorted(list(set(ice) - set(melt_lst)))
     year += 1
 
-if group < 2:
-    print(0)
+if n < 2: print(0)
