787dc4f3fb728f2fba666fc521ecd9ad4b7e41c1
yunzo <139421060+2yunj007@users.noreply.github.com>
2023-12-11 19:40:09 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 516 ms, Memory: 112556 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -8,12 +8,6 @@ dp = [[0] * N for _ in range(N)]
 
 for i in range(2, N+1):    # 부분 행렬의 길이
     for j in range(N-i+1):   # 시작점
-        dp[j][j+i-1] = int(1e9)  # 지금 계산할 첫행렬과 끝행렬
+        dp[j][j+i-1] = min([dp[j][j+k] + dp[j+k+1][j+i-1] + arr[j][0] * arr[j+k][1] * arr[j+i-1][1] for k in range(i-1)])
 
-        for k in range(i-1):
-            dp[j][j+i-1] = min(
-                dp[j][j+i-1],
-                dp[j][j+k] + dp[j+k+1][j+i-1] + arr[j][0] * arr[j+k][1] * arr[j+i-1][1]
-            )
-
-print(dp[0][N - 1])
+print(dp[0][N-1])
