72d236769a20e29de0d88636643a7a6a517f94a4
yunzo <139421060+2yunj007@users.noreply.github.com>
2024-06-09 18:27:46 +0900

    [Silver I] Title: 계산 로봇, Time: 784 ms, Memory: 206984 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/22342. 계산 로봇/계산 로봇.py
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.py
@@ -1,26 +1,21 @@
-M, N = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(M)]
+import sys
+input = sys.stdin.readline
 
-answer = 0
+M, N = map(int,input().split())
 
-save = [[0 for _ in range(N)] for _ in range(M)]    # 범위 내 로봇들의 출력 값 중 최댓값
-output = [[0 for _ in range(N)] for _ in range(M)]  # 저장 값 + 가중치
+D = [list(map(int,input().strip())) for _ in range(M)]
 
-# 가장 왼쪽에 있는 열의 입력 값은 0이므로 저장 값은 0, 출력 값은 가중치가 됨
-for i in range(M):
-    output[i][0] = arr[i][0]
+save = [[0] * (N+2) for _ in range(M+2)]
+output = [[0] * (N+2) for _ in range(M+2)]
 
-for j in range(1, N):
-    for i in range(M):
-        # (i, j)의 저장 값은 범위 내의 로봇 중 j-1 열에 있는 저장 값들의 최댓갑
-        if i == 0:
-            save[i][j] = max(output[i][j - 1], output[i + 1][j - 1])
-        elif i == M - 1:
-            save[i][j] = max(output[i - 1][j - 1], output[i][j - 1])
-        else:
-            save[i][j] = max(output[i + 1][j - 1], output[i][j - 1], output[i - 1][j - 1])
+for i in range(1,M+1):
+    output[i][1] = D[i-1][0]
 
-        output[i][j] = save[i][j] + arr[i][j]
-        answer = max(answer, save[i][j])
+result = 0
+for j in range(1,N+1):
+    for i in range(1,M+1):
+        save[i][j] = max(output[i-1][j-1],output[i][j-1],output[i+1][j-1])
+        output[i][j] = save[i][j] + D[i-1][j-1]
+        result = max(save[i][j],result)
 
-print(answer)
+print(result)
