6f68b77fb767a2b1d9107f781bba93ccf27910de
yunzo <139421060+2yunj007@users.noreply.github.com>
2024-06-09 18:20:32 +0900

    [Silver I] Title: 계산 로봇, Time: 124 ms, Memory: 31120 KB, Score: 11 point -BaekjoonHub

---
+++ 백준/Silver/22342. 계산 로봇/계산 로봇.py
@@ -0,0 +1,27 @@
+M, N = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(M)]
+
+save = [[0 for _ in range(N)] for _ in range(M)]    # 범위 내 로봇들의 출력 값 중 최댓값
+output = [[0 for _ in range(N)] for _ in range(M)]  # 저장 값 + 가중치
+
+# 가장 왼쪽에 있는 열의 입력 값은 0이므로 저장 값은 0, 출력 값은 가중치가 됨
+for i in range(M):
+    output[i][0] = arr[i][0]
+
+for j in range(1, N):
+    for i in range(M):
+        # (i, j)의 저장 값은 범위 내의 로봇 중 j-1 열에 있는 저장 값들의 최댓갑
+        if i == 0:
+            save[i][j] = max(output[i][j - 1], output[i + 1][j - 1])
+        elif i == M - 1:
+            save[i][j] = max(output[i - 1][j - 1], output[i][j - 1])
+        else:
+            save[i][j] = max(output[i + 1][j - 1], output[i][j - 1], output[i - 1][j - 1])
+
+        output[i][j] = save[i][j] + arr[i][j]
+
+answer = 0
+for i in range(M):
+    answer = max(answer, max(save[i]))
+
+print(answer)
